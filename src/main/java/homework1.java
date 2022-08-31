8118045876524c30ecff96d67f5ca78a2c590ec3
AnastasMihailova <109463973+AnastasMihailova@users.noreply.github.com>
2022-09-01 01:17:24 +0300

    Add files via upload

--- src/main/java/homework1.java
+++ src/main/java/homework1.java
@@ -1,22 +1,44 @@
 import java.util.Random;
 
 public class homework1 {
+     public static void main(String[] args) {
+            int[][] num = new int[5][5];
+            Random rnd = new Random();
+            for (int i = 0; i < num.length; i++) {
+                for (int j = 0; j < num[i].length; j++) {
+                    num[i][j] = rnd.nextInt(10) + 1;
+                }
+            }
+            for (int i = 0; i < num.length; i++, System.out.println()) {
+                for (int j = 0; j < num[i].length; j++) {
+                    System.out.print(num[i][j] + " ");
+                }
+            }
+            int max = num[0][0];
+            for (int i = 0; i < num.length; i++) {
+                if (max < num[num.length - 1 - i][i]) {
+                    max = num[num.length - 1 - i][i];
+                }
+            }
+            System.out.println(max);
+            int min = num[0][0];
+            for (int i = 0; i < num.length; i++) {
+                min = Math.min(num[i][i], min);
+            }
+            System.out.println(min);
 
- public static void main(String[] args) {
-        int[][] array = new int[10][20];
-        Random random = new Random();
-        for (int i = 0; i < array.length; i++){
-            for (int j = 0; j < array[i].length; j++){
-                array[i][j] = random.nextInt(30) + 1;
+
+            int delta = Integer.MAX_VALUE;
+            int index = 0;
+            int aver = (max + min) / 2;
+            for (int i = 0; i < num.length; i++) {
+                int d = Math.abs(num[i][i] - aver);
+                if (d < delta) {
+                    delta = d;
+                    index = i;
+                }
             }
-        }
-        for(int i = 0; i < array.length; i++){
-            for (int j = 0; j < array[i].length; j++){
-                System.out.print(array[i][j]+" ");
-            }
-            System.out.println();
+            System.out.println("среднее арифметическое чисел равно: " + num[index]);
         }
     }
- }
-
 
